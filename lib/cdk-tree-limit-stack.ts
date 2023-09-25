da1cfd9fde38bb86f74bb0b7794b7a8b033ef4ea
Bryan Moffatt <moffattb@amazon.com>
2023-09-25 15:41:53 -0700

    Make it explode

--- lib/cdk-tree-limit-stack.ts
+++ lib/cdk-tree-limit-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkTreeLimitStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,8 +9,10 @@ export class CdkTreeLimitStack extends cdk.Stack {
     // The code that defines your stack goes here
 
     // example resource
-    // const queue = new sqs.Queue(this, 'CdkTreeLimitQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    for (let i = 0; i < 101; i++) {
+        const queue = new sqs.Queue(this, `CdkTreeLimitQueue-${i}`, {
+           visibilityTimeout: cdk.Duration.seconds(300)
+        });
+    }
   }
 }
